5113b0a54f7cdf357840442e45fcb74be65c14dd
HAKan100551 <56893768+HAKan100551@users.noreply.github.com>
2019-10-23 18:19:28 +0300

    Update HelloGitHub.java

--- Java/HelloGitHub.java
+++ Java/HelloGitHub.java
@@ -3,6 +3,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 
 	    System.out.println("Hello, World!");
+	    System.out.println("Hello, Github!");
 	    
 	    //Yukarıdaki komutun sözdizimini kullanarak aşağıya "Hello, GitHub!" yazdırın.
 
